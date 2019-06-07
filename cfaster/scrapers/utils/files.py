9da45b04be665a6015b1c29b790baf92e9407785
Vitor Falcao <vitorfhcosta@gmail.com>
2019-06-07 10:27:42 -0300

    Inputs and outputs created inside specific folders
    Closes #2

--- cfaster/scrapers/utils/files.py
+++ cfaster/scrapers/utils/files.py
@@ -1,16 +1,23 @@
 import logging
+from os import mkdir
 
 logger = logging.getLogger(__name__)
 
 
 def file_saver(inputs, outputs):
+    in_folder = 'inputs/'
+    out_folder = 'outputs/'
+    mkdir(in_folder)
+    mkdir(out_folder)
+
     for ind, inp in enumerate(inputs):
-        filename = f'input_{ind+1}.txt'
-        logger.info('Writing to ' + filename)
+        filename = f'{in_folder}input_{ind+1}.txt'
+        logger.debug('Writing to ' + filename)
         with open(filename, 'w') as f:
             f.write(inp)
+
     for ind, out in enumerate(outputs):
-        filename = f'output_{ind+1}.txt'
-        logger.info('Writing to ' + filename)
+        filename = f'{out_folder}output_{ind+1}.txt'
+        logger.debug('Writing to ' + filename)
         with open(filename, 'w') as f:
             f.write(out)
