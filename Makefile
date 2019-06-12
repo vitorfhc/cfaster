42e6f1585638107e1368cc3ffeec88e5b234d610
Vitor Falcao <vitorfhcosta@gmail.com>
2019-06-12 13:23:01 -0300

    Improves scripts and urls.py

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ package:
 	python setup.py sdist
 	python setup.py bdist_wheel
 
-upload_test: clean package
+upload-test: clean package
 	twine upload --repository-url https://test.pypi.org/legacy/ dist/*
 
 upload: clean package
@@ -12,6 +12,12 @@ upload: clean package
 
 install:
 	python setup.py install
+
+install-test:
+	pip install --index https://test.pypi.org/simple --extra-index https://test.pypi.org/simple --upgrade cfaster
+
+install-prod:
+	pip install --upgrade cfaster
 
 clean:
 	rm -rf dist build *.egg-info
--- cfaster/scrapers/utils/urls.py
+++ cfaster/scrapers/utils/urls.py
@@ -5,12 +5,13 @@ logger = logging.getLogger(__name__)
 
 
 def validate_url(url, expected_domain):
-    logger.info('Validating url {} with expected domain {}'.format(url, expected_domain))
+    logger.info('Validating url {} with expected domain {}'.format(
+        url, expected_domain))
     url = url.strip()
     reg = r'^(?:https?://)?(?:www.)?(\w+)(?:.\w+)+(?:/\w*)*$'
     match = re.search(reg, url)
     if match is None:
         return False
-    if match[1] == expected_domain:
+    if len(match.groups()) and match.group(1) == expected_domain:
         return True
     return False
